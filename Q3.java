5c8ef9469a97b963b4cdb828625189fc12fbbc5f
Adv <advaitesha.gupta15072003@gmail.com>
2022-09-13 00:41:34 +0530

    ALL FILES ARE ADDED.

---
+++ Q3.java
@@ -0,0 +1,58 @@
+package lab_4;
+import java.util.Scanner;
+public class Q3 {
+	public static void main(String[]args) {
+	System.out.println("Enter the no range from (1-12): ");
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	switch(a) {
+	case(1):{
+		System.out.println("January");
+		break;
+	}
+	case(2):{
+		System.out.println("February");
+		break;
+		}
+	case(3):{
+		System.out.println("March");
+		break;
+		}
+	case(4):{
+		System.out.println("April");
+		break;
+	}
+	case(5):{
+		System.out.println("May");
+		break;
+		}
+	case(6):{
+		System.out.println("June");
+		break;
+		}
+	case(7):{
+		System.out.println("July");
+		break;
+		}
+	case(8):{
+		System.out.println("August");
+		break;
+		}
+	case(9):{
+		System.out.println("September");
+		break;
+		}
+	case(10):{
+		System.out.println("October");
+		break;	
+	}
+	case(11):{
+		System.out.println("November");}
+	case(12):{
+		System.out.println("Decemebr");
+		break;
+		}
+	}
+
+}
+	}
---
+++ Q4.java
@@ -0,0 +1,15 @@
+package lab_4;
+public class Q4 {
+public static void main(String[]args) {
+	int i;
+	int sum=0;
+	for (i=41;i<250;i++) {
+		if (i%5==0) {
+			sum+=i;
+		}
+	}
+	System.out.println("sum of all numbers divisible by 5 is: " +sum );	
+	
+	}
+}
+
